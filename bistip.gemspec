4c11b141c7fd83b39008f19101decd042fbe009f
Agung Prasetyo <brain64bit@gmail.com>
2011-09-24 20:49:52 +0700

    bistip ruby gem

--- bistip.gemspec
+++ bistip.gemspec
@@ -6,16 +6,23 @@ Gem::Specification.new do |s|
   s.name        = "bistip"
   s.version     = Bistip::VERSION
   s.platform    = Gem::Platform::RUBY
-  s.authors     = ["TODO: Write your name"]
-  s.email       = ["TODO: Write your email address"]
-  s.homepage    = ""
-  s.summary     = %q{TODO: Write a gem summary}
-  s.description = %q{TODO: Write a gem description}
-
+  s.authors     = ["Agung Prasetyo"]
+  s.email       = ["brain64bit@gmail.com"]
+  s.extra_rdoc_files = ["MIT-LICENSE","README"]
+  s.rdoc_options = ["--charset=UTF-8"]
+  s.homepage    = "http://kav1nsky.wordpress.com"
+  s.summary     = %q{A Ruby wrapper based on Bistip.com API}
+  s.description = %q{Ruby wrapper that consume Bistip.com REST API, for searching trips and searching wanted post specified by some parameter}
+  s.date = Time.now.utc.strftime("%A, %d% %B %Y")
   s.rubyforge_project = "bistip"
+  
+  s.add_dependency "httparty", "= 0.7.8"
 
+  s.post_install_message = "Get tip for your trip"
+  
   s.files         = `git ls-files`.split("\n")
   s.test_files    = `git ls-files -- {test,spec,features}/*`.split("\n")
   s.executables   = `git ls-files -- bin/*`.split("\n").map{ |f| File.basename(f) }
   s.require_paths = ["lib"]
+  
 end
--- lib/bistip.rb
+++ lib/bistip.rb
@@ -1,3 +1,50 @@
+require 'httparty'
+
 module Bistip
+  include HTTParty
   # Your code goes here...
+  
+  base_uri 'http://www.bistip.com'
+
+  API_VERSION = '/api/v1'
+  SEARCH_TRIP = '/trips.json'
+  SEARCH_SEEK = '/seeks.json'
+  DEFAULT_PER_PAGE = 10
+  DEFAULT_PAGE = 1
+  
+  def self.trips(params={})
+  	check_params(params)
+  	from = params[:from] if params[:from]
+  	to = params[:to] if params[:to]
+  	per_page = params[:per_page].nil? ? DEFAULT_PER_PAGE : params[:per_page]
+  	page = params[:page].nil? ? DEFAULT_PAGE : params[:page]
+  	
+  	result = get("#{API_VERSION+SEARCH_TRIP}?from=#{from}&to=#{to}&page=#{page}&per_page=#{per_page}")
+	result = result.first.include?("exception") ? Array.new : result
+	return result
+  end
+  
+  def self.seeks(params={})
+  	check_params(params)
+  	from = params[:from] if params[:from]
+  	to = params[:to] if params[:to]
+  	per_page = params[:per_page].nil? ? DEFAULT_PER_PAGE : params[:per_page]
+  	page = params[:page].nil? ? DEFAULT_PAGE : params[:page]
+  	
+  	result = get("#{API_VERSION+SEARCH_SEEK}?from=#{from}&to=#{to}&page=#{page}&per_page=#{per_page}")
+	result = result.first.include?("exception") ? Array.new : result
+	return result
+  end
+  
+  private
+  
+  	def self.check_params(params)
+  		raise ArgumentError, 'parameters must be a hash' unless params.is_a?(Hash)
+  		raise ArgumentError, 'parameters can not be empty' unless !params.empty?
+  		raise ArgumentError, 'parameters should contain :from and/or :to properties' unless params.keys.include?(:from) or params.keys.include?(:to)
+  		
+  		raise ArgumentError, ':page must be an integer' unless params[:page].is_a?(Integer) if params[:page]
+  		raise ArgumentError, ':per_page must be an integer' unless params[:per_page].is_a?(Integer) if params[:per_page]
+  	end
+  
 end
